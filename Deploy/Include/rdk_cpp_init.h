a4157fb4584d66319ae46c86b9a0ca46e68a4f03
Alexab <devnull@localhost>
2012-06-05 12:06:46 +0400

    rdk_cpp_init: Comments added.

--- Deploy/Include/rdk_cpp_init.h
+++ Deploy/Include/rdk_cpp_init.h
@@ -6,9 +6,16 @@
 
 extern "C++"  {
 
+// ¬озвращает ссылку на указатель управл€ющего €дра
 RDK_LIB_TYPE RDK::UEPtr<RDK::UEngine>& RDK_CALL GetEngine(void);
+
+// ¬озвращает ссылку на указатель среды выполнени€
 RDK_LIB_TYPE RDK::UEPtr<RDK::UAEnvironment>& RDK_CALL GetEnvironment(void);
+
+// ¬озвращает ссылку на указатель хранилища
 RDK_LIB_TYPE RDK::UEPtr<RDK::UAStorage>& RDK_CALL GetStorage(void);
+
+// ¬озвращает указатель на текущую модель
 RDK_LIB_TYPE RDK::UEPtr<RDK::UAContainer> RDK_CALL GetModel(void);
 }
 
