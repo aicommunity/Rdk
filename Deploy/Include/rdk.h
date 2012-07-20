3b162fc7b76e3e7fafdd3ee19c198e559f6520d3
Alexab <devnull@localhost>
2012-07-20 13:18:13 +0400

    Added some comments

--- Deploy/Include/rdk.h
+++ Deploy/Include/rdk.h
@@ -1,3 +1,16 @@
+/****************************************************************************/
+/*! \mainpage RDK library
+ * \section intro_sec Введение
+ *
+ *      Пожалуйста прочтите следующую документацию (каталог Docs):
+ *      * Назначение библиотеки: RDK.Purposes.Ru.doc
+ *      * Структура файлов библиотеки: RDK.FileStructure.Ru.doc
+ *      * Быстрый старт. Использование в проекте. RDK.QuickStart.UsingToProject.Ru.doc
+ *      * Быстрый старт. Разработка конечного компонента. RDK.QuickStart.ComponentDevelopment.Ru.doc
+ *
+ * @version     V0.1.1
+ * @author      Aleksandr Bakhshiev
+ ****************************************************************************/
 #ifndef RDK_H
 #define RDK_H
 
