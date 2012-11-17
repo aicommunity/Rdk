3d2d4fdfd654507985d1a0c0e1d9280a263772f9
Alexab <palexab@gmail.com>
2012-11-17 12:43:13 +0400

    Deploy: #include bugs fixed

--- Deploy/Include/rdk.bcb.cpp
+++ Deploy/Include/rdk.bcb.cpp
@@ -2,7 +2,7 @@
 #define RDK_WINAPI
 #include "rdk_exceptions.bcb.cpp"
 #include "rdk.cpp"
-#include "../../Core/System/BCB/rdk_system.bcb.cpp"
+//#include "../../Core/System/BCB/rdk_system.bcb.cpp"
 
 
 
--- Deploy/Include/rdk.gcc.cpp
+++ Deploy/Include/rdk.gcc.cpp
@@ -1,5 +1,4 @@
 #define RDK_GCC
 #include "rdk_exceptions.gcc.cpp"
 #include "rdk.cpp"
-#include "../../Core/System/Gcc/rdk_system.gcc.cpp"
 
