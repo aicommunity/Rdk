13c30dbf3d73c8ad7ca2e627b307b0f8a62219ed
Alexab <palexab@gmail.com>
2012-12-01 12:04:04 +0400

    Continued changes in the directory structure: Binaries library renamed from rdk.xxx.yyy in librdk.xxx.yyy Static libraries have the suffix .static.lib

--- Deploy/Include/myrdk.bcb.cpp
+++ Deploy/Include/myrdk.bcb.cpp
@@ -1,4 +1,5 @@
-#include "../../Core/System/Win/rdk_system.win.cpp"   
+#include "../../Core/System/Win/rdk_system.win.cpp"
 #include "myrdk.cpp"
 #include "../../GUI/BCB/TUBitmap.cpp"
+#include "../../GUI/BCB/TUFileSystem.cpp"
 
--- Deploy/Include/rdk.win.cpp
+++ Deploy/Include/rdk.win.cpp
@@ -3,6 +3,6 @@
 #pragma warning( disable : 4355 )  
 #pragma warning( disable : 4800 )
 #include "rdk_exceptions.win.cpp"
-#include "../../Core/System/Win/rdk_system.win.cpp"
+//#include "../../Core/System/Win/rdk_system.win.cpp"
 #include "rdk.cpp"
 
