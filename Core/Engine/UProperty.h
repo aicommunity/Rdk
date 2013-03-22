2be8e856547f66033349ae286faa3e23a955e14f
Alexab <palexab@gmail.com>
2013-03-22 15:20:53 +0400

    Core: Graphics/Libraries.cpp now should be added in project independently.

--- Core/Engine/UProperty.h
+++ Core/Engine/UProperty.h
@@ -23,6 +23,8 @@ See file license.txt for more information
 #include "../Serialize/UBinaryStdSerialize.h"
 #include "../Graphics/UGraphicsBinarySerialize.h"
 #include "../Graphics/UGraphicsXMLSerialize.h"
+#include "../Math/MUXMLSerialize.h"
+#include "../Math/MUBinarySerialize.h"
 #include "UComponent.h"
 
 namespace RDK {
