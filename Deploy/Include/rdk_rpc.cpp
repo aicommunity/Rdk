0a4a25ff77b18a2cbaa2c4f34cee0985bb11b867
Alexab <palexab@gmail.com>
2013-08-09 20:21:46 +0400

    Added RPC implementation files.

---
+++ Deploy/Include/rdk_rpc.cpp
@@ -0,0 +1,19 @@
+#ifndef RDK_RPC_CPP
+#define RDK_RPC_CPP
+#include "rdk_rpc.h"
+
+
+namespace RDK{
+
+const char* RemoteCallInternal(const char *request, int &return_value)
+{
+ return_value=0;
+
+ return 0;
+}
+
+
+}
+
+#endif
+
---
+++ Deploy/Include/rdk_rpc.h
@@ -0,0 +1,12 @@
+#ifndef rdk_rpcH
+#define rdk_rpcH
+
+#include "rdk_init.h"
+
+namespace RDK{
+
+const char* RemoteCallInternal(const char *request, int &return_value);
+
+}
+
+#endif
