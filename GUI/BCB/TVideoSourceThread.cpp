68ea1c4848892ce804d9ac05ba321c5a2e3188d8
Alexab <palexab@gmail.com>
2014-12-05 17:53:01 +0300

    Gui/Bcb/TVideoSourceThread: Debug - reconnect of the video capturing disabled.

--- GUI/BCB/TVideoSourceThread.cpp
+++ GUI/BCB/TVideoSourceThread.cpp
@@ -382,7 +382,7 @@ void __fastcall TVideoCaptureThread::Execute(void)
    continue;
   }
   ProcessCommandQueue();
-
+/*
   double curr_time=TDateTime::CurrentDateTime().operator double();
   if(curr_time-RealLastTimeStamp>double(MaxInterstepInterval)/(86400.0*1000.0))
   {
@@ -437,7 +437,7 @@ void __fastcall TVideoCaptureThread::Execute(void)
    SetEvent(FrameNotInProgress);
    continue;
   }
-
+    */
   if(SyncMode == 1)
   {
    if(WaitForSingleObject(CalcCompleteEvent,10) == WAIT_TIMEOUT)
