44ed6252b26f0255348cc6f35412d452ff808c59
Alexab <devnull@localhost>
2015-03-08 00:19:30 +0300

    Some linux compatibility problems fixed #3

--- Core/System/Gcc/UGenericMutex.gcc.cpp
+++ Core/System/Gcc/UGenericMutex.gcc.cpp
@@ -23,6 +23,25 @@ virtual bool exclusive_unlock(void);
 virtual bool wait(int timeout);
 };
 
+class RDK_LIB_TYPE UGenericEventGcc: public UGenericEvent
+{
+protected:
+//HANDLE Event;
+
+public:
+UGenericEventGcc();
+virtual ~UGenericEventGcc();
+
+virtual bool set(void);
+virtual bool reset(void);
+virtual bool wait(unsigned wait_time);
+
+
+private:
+UGenericEventGcc(const UGenericEventGcc &copy);
+UGenericEventGcc& operator = (const UGenericEventGcc &copy);
+};
+
 
 UGenericMutexGcc::UGenericMutexGcc()
 {
@@ -63,6 +82,47 @@ bool UGenericMutexGcc::wait(int timeout)
  } */
  return false;
 }
+
+UGenericEventGcc::UGenericEventGcc()
+{
+ //Event=CreateEvent(0,FALSE,TRUE,0);
+}
+
+UGenericEventGcc::~UGenericEventGcc()
+{
+ //if(Event)
+ // CloseHandle(Event);
+}
+
+bool UGenericEventGcc::set(void)
+{
+ //SetEvent(Event);
+ return true;
+}
+
+bool UGenericEventGcc::reset(void)
+{
+ //ResetEvent(Event);
+ return true;
+}
+
+bool UGenericEventGcc::wait(unsigned wait_time)
+{
+ //if(WaitForSingleObject(Event,wait_time) == WAIT_TIMEOUT)
+ // return false;
+ return true;
+}
+
+UGenericEventGcc::UGenericEventGcc(const UGenericEventGcc &copy)
+{
+
+}
+
+UGenericEventGcc& UGenericEventGcc::operator = (const UGenericEventGcc &copy)
+{
+ return *this;
+}
+
 // ---------------------------------------------------------------------------
 UGenericMutex* UCreateMutex(void)
 {
@@ -73,6 +133,17 @@ void UDestroyMutex(UGenericMutex* mutex)
 {
  if(mutex)
   delete mutex;
+}
+
+UGenericEvent* UCreateEvent(void)
+{
+ return new UGenericEventGcc;
+}
+
+void UDestroyEvent(UGenericEvent* event)
+{
+ if(event)
+  delete event;
 }
 
 #endif
