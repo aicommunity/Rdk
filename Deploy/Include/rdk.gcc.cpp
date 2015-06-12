a8d6572a498dc854891f811aae82653d5e332e39
Alexab <devnull@localhost>
2015-06-12 13:31:08 +0300

    Added & fixed CodeBlocks projects.

--- Deploy/Include/rdk.gcc.cpp
+++ Deploy/Include/rdk.gcc.cpp
@@ -1,9 +1,3 @@
 #define RDK_GCC
 #include "rdk_exceptions.gcc.cpp"
 #include "rdk.cpp"
-#include "../../Core/Engine/Libraries/UELibraries.cpp"
-#include "../../Core/Graphics/Libraries/Libraries.cpp"
-#include "../../Core/Graphics/Libraries/Detection/Detection.cpp"
-#include "../../Core/Graphics/Libraries/SpatialGeometry/SpatialGeometry.cpp"
-#include "../../Core/Graphics/Libraries/Hardware/Hardware.cpp"
-
