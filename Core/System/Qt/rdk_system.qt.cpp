5dde8b552631cc4620d66d4453e832827af6df0c
Alexab <palexab@gmail.com>
2016-02-28 18:03:52 +0300

    Qt files have been updated (in progress).

--- Core/System/Qt/rdk_system.qt.cpp
+++ Core/System/Qt/rdk_system.qt.cpp
@@ -46,6 +46,7 @@ unsigned long long CalcDiffTime(unsigned long long time1, unsigned long long tim
 /// Возвращает локальное время в днях (с точностью до миллисекунд) от начала времен
 double GetVariantLocalTime(void)
 {
+ return 0.0;
 // TODO:
 }
 
--- Deploy/Include/rdk.qt.cpp
+++ Deploy/Include/rdk.qt.cpp
@@ -1,6 +1,6 @@
 #ifndef RDK_QT_CPP
 #define RDK_QT_CPP
-
+/*
 #ifdef __GNUC__
 #include "rdk.gcc.cpp"
 #elif defined WIN32
@@ -8,5 +8,5 @@
 #else
 #include "rdk.ansi.cpp"
 #endif
-
+*/
 #endif
