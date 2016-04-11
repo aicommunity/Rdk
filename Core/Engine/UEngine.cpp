fe43f075f5b19a4cf50e42d85610112d407a88af
APopova <devnull@localhost>
2016-04-11 15:50:01 +0300

    Fixed method Model_LoadComponent

--- Core/Engine/UEngine.cpp
+++ Core/Engine/UEngine.cpp
@@ -5056,6 +5056,7 @@ int UEngine::Model_LoadComponent(const char *stringid, const char* buffer)
   {
    AccessCache.clear();
    XmlStorage.Load(buffer,"Save");
+   std::string xml_model_name=XmlStorage.GetNodeAttribute("ModelName");
    XmlStorage.SelectNode(0);
 
    Environment->Reset();
@@ -5077,8 +5078,8 @@ int UEngine::Model_LoadComponent(const char *stringid, const char* buffer)
    }
    else
    {
-	if((strlen(stringid) == 0) && (XmlStorage.GetNodeAttribute("ModelName") != Environment->GetModel()->GetName()))
-	 RDK_RAW_THROW(EErrorEngineModelNameDontMatch(XmlStorage.GetNodeAttribute("ModelName"), Environment->GetModel()->GetName()));
+	if((strlen(stringid) == 0) && (xml_model_name != Environment->GetModel()->GetName()))
+	 RDK_RAW_THROW(EErrorEngineModelNameDontMatch(xml_model_name, Environment->GetModel()->GetName()));
 
 	UEPtr<RDK::UNet> cont=dynamic_pointer_cast<RDK::UNet>(FindComponent(stringid));
 
