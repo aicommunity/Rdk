b3f809a8fdfb772dbf2a1cd034f20d78486c451b
Alexab <palexab@gmail.com>
2017-12-05 19:38:15 +0300

    Delayed application startup has been added.

--- GUI/BCB/UGEngineControlFormUnit.cpp
+++ GUI/BCB/UGEngineControlFormUnit.cpp
@@ -90,6 +90,8 @@ String Lang_Starting("Starting...");
 String Lang_Stopping("Stopping...");
 String Lang_ApplicationRun("Launching application. Please Wait...");
 String Lang_ApplicationClose("Closing application. Please Wait...");
+String Lang_StartupDelay("Application launch delayed to");
+String Lang_StartupDelaySuffix("seconds");
 
 TUVisualControllerForm *RdkMainForm=0;
 
@@ -214,7 +216,7 @@ void CreateStatusWindow(const String &Text)
 						 (Screen->Height - FormHeight) / 2,
 						 FormWidth,
 						 FormHeight,
-						 Application->MainForm->Handle,
+						 0,//Application->MainForm->Handle,
 						 0,
 						 GetModuleHandle(0),
 						 0);
@@ -2404,6 +2406,14 @@ void __fastcall TUGEngineControlForm::FormCreate(TObject *Sender)
  ProgramName=app_ini->ReadString("General","ProgramName","Server");
  NeverSleepOnMMThreadContention=app_ini->ReadBool("General","NeverSleepOnMMThreadContention",false);
  LogDir=app_ini->ReadString("Log","Dir","");
+ StartupDelay=app_ini->ReadInteger("General","StartupDelay",0);
+
+ if(StartupDelay>0)
+ {
+  RDK::CreateStatusWindow(Lang_StartupDelay+String(" ")+IntToStr(StartupDelay/1000)+String(" ")+Lang_StartupDelaySuffix);
+  Sleep(StartupDelay);
+  RDK::RemoveStatusWindow();
+ }
 
  if(LogDir.Length() == 0)
   LogDir = "EventsLog/";
