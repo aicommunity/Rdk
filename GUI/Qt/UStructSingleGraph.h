ca53a61887590b5196347bf0d62a360635c65597
tseluyko <devnull@localhost>
2018-10-03 14:20:19 +0300

    fix encoding and fix include QtPrintSupport

--- GUI/Qt/UStructSingleGraph.h
+++ GUI/Qt/UStructSingleGraph.h
@@ -10,7 +10,7 @@ struct TSingleGraph
     QColor t_graphColor;       //Цвет графика
     std::string t_graphName;   //Имя графика
     int indexChannel;          //Индекс канала
-    std::string nameCompоnent; //Имя компонента
+    std::string nameComponent; //Имя компонента
     std::string nameProperty;  //Имя свойства
     std::string typeProperty;  //Тип компонента
     //координаты элемента матрицы
