689515ed0f2aed1a130ee3ef0cfaa8de04bcd133
Alexab-GPD <devnull@localhost>
2020-03-07 17:42:18 +0300

    Debug.

--- GUI/Qt/UWatchFormWidget.h
+++ GUI/Qt/UWatchFormWidget.h
@@ -15,6 +15,9 @@ public:
  explicit UWatchFormWidget(QWidget *parent = nullptr);
  ~UWatchFormWidget();
 
+private slots:
+    void on_pushButton_2_clicked();
+
 private:
  Ui::UWatchFormWidget *ui;
 };
