efb40598b37e63cf0c071cc71aca0325c4a9b06b
Aivan_2008 <devnull@localhost>
2020-04-09 13:27:12 +0300

    New files added

---
+++ Core/Application/UServerTransportHttp.cpp
@@ -0,0 +1,29 @@
+#include "UServerTransportHttp.h"
+
+namespace RDK{
+// --------------------------
+//  онструкторы и деструкторы
+// --------------------------
+UServerTransportHttp::UServerTransportHttp(void)
+{
+
+}
+UServerTransportHttp::~UServerTransportHttp(void)
+{
+
+}
+// --------------------------
+
+// --------------------------
+// ћетоды транспортировки данных
+// --------------------------
+
+/// „итает вход€щие байты из выбранного источника, контекст прив€зки
+/// всегда определ€етс€ строкой вне зависимости от типа транспорта
+//int UServerTransportHttp::ReadIncomingBytes(std::string &bind, std::vector<unsigned char> &bytes)
+//{
+
+//}
+
+
+}//namespace RDK
