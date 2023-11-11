2da34287a0b4a5d474a2e87d5bdbd2d2c956f678
Alexab <devnull@localhost>
2023-11-11 15:08:33 +0300

    Upd MtWorker

--- Core/Engine/UMtWorker.cpp
+++ Core/Engine/UMtWorker.cpp
@@ -1,1 +1,71 @@
 #include "UMtWorker.h"
+
+namespace RDK {
+
+UMtWorker::UMtWorker(UEPtr<UContainer> owner)
+ : Owner(owner)
+ , IsProcessing(true)
+{
+ SleepMutex.unlock();
+ CompleteMutex.unlock();
+ Thread = std::thread(std::bind(&UMtWorker::Process,this));
+}
+
+UMtWorker::~UMtWorker(void)
+{
+ IsProcessing = false;
+ WaitForComplete();
+}
+
+void UMtWorker::AddProcessingComponent(const std::string &name)
+{
+ if(!Owner)
+  return;
+
+ if(ProcessingComponents.find(name) != ProcessingComponents.end())
+  return;
+
+ std::unique_lock<std::mutex> locker(SleepMutex);
+
+ UEPtr<UContainer> cont = Owner->GetComponent(name,true);
+
+ if(cont)
+  ProcessingComponents[name] = cont;
+}
+
+void UMtWorker::ClearAllProcessingComponents(void)
+{
+ std::unique_lock<std::mutex> locker(SleepMutex);
+ ProcessingComponents.clear();
+}
+
+void UMtWorker::Init(void)
+{
+
+}
+
+void UMtWorker::UnInit(void)
+{
+
+}
+
+void UMtWorker::Calculate(void)
+{
+
+}
+
+void UMtWorker::WaitForComplete(void)
+{
+
+}
+
+void UMtWorker::Process(void)
+{
+ while(IsProcessing)
+ {
+  SleepMutex.lock();
+  std::this_thread::sleep_for(1ms);
+ }
+}
+
+}
--- Core/Engine/UMtWorker.h
+++ Core/Engine/UMtWorker.h
@@ -1,4 +1,47 @@
 #ifndef UMTWORKER_H
 #define UMTWORKER_H
 
+#include <thread>
+#include <mutex>
+#include <map>
+#include <string>
+#include <functional>
+#include "UEPtr.h"
+#include "UContainer.h"
+
+namespace RDK {
+
+class UMtWorker
+{
+protected:
+ UEPtr<UContainer> Owner;
+
+ std::map<std::string, UEPtr<UContainer>> ProcessingComponents;
+
+private:
+ std::thread Thread;
+
+ std::mutex SleepMutex;
+ std::mutex CompleteMutex;
+ std::atomic_bool IsProcessing;
+
+public:
+ UMtWorker(UEPtr<UContainer> owner);
+ ~UMtWorker(void);
+
+ void AddProcessingComponent(const std::string &name);
+ void ClearAllProcessingComponents(void);
+
+ void Init(void);
+ void UnInit(void);
+
+ void Calculate(void);
+ void WaitForComplete(void);
+
+protected:
+ void Process(void);
+};
+
+}
+
 #endif
